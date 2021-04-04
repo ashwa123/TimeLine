26c765bdb2f8b2846873f7fe4bd7904516095465
Ashwanth <ashwanth23.2.98@gmail.com>
2021-04-04 18:48:16 +0530

    seventeen commit

--- timeLine/src/app/home/home.component.ts
+++ timeLine/src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Meta } from '@angular/platform-browser';
 import './home.js';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FeedbackService } from '../feedback/feedback.service.js';
 
 declare var navSlide: any;
 
@@ -16,14 +18,23 @@ export class HomeComponent implements OnInit {
   // @ViewChild('navbarTogglerDemo02') navbarTogglerDemo02: ElementRef
 
   LogoUrl = '../../assets/Logo.png';
+  feedbackForm: FormGroup;
 
 
   constructor(
     private router: Router,
+    private fb: FormBuilder,
+    private service: FeedbackService,
     private metaTagService: Meta
   ) { }
 
   ngOnInit(): void {
+    this.feedbackForm = this.fb.group({
+      name : ['', [Validators.required]],
+      email : ['', [Validators.required]],
+      comments : ['', [Validators.required]]
+    });
+
     this.metaTagService.addTags([
       { name: 'description', content: 'Angular SEO Integration, Music CRUD, Angular Universal' },
       { name: 'keywords', content: 'index, follow' },
